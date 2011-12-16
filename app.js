e92704db3f42343d8204c514a6da5c8c309ab34e
Atsushi Nakatsugawa <atsushi@moongift.jp>
2011-12-16 21:47:12 +0900

    Save to mongodb

--- app.js
+++ app.js
@@ -2,39 +2,61 @@ var ext = require('./extract-content-all'),
     ex = new ext.ExtractContentJS.LayeredExtractor(),
     request = require('request'),
     jsdom = require('jsdom'),
-    Iconv  = require('iconv').Iconv,
     app = require('express').createServer(),
+    mongoose = require('mongoose'),
+    Iconv  = require('iconv').Iconv,
     Buffer = require('buffer').Buffer;
 
-ex.addHandler(ex.factory.getHandler('Heuristics'));
-url = 'http://www.aichan.biz/html/html/h.html';
+// url = 'http://www.aichan.biz/html/html/h.html';
 // url = 'http://d.hatena.ne.jp/hokaccha/20110801/1312216630'
 
-/*
-        var iconv  = new Iconv("Shift-JIS", 'UTF-8//TRANSLIT//IGNORE');
-        console.log((iconv.convert(res.title)).toString());
-        console.log((iconv.convert(res.content.toString())).toString());
-        console.log(res.content.main_image(jsdom));
-*/
+var Schema   = mongoose.Schema;
+var UrlSchema = new Schema({
+    title: String,
+    content: String,
+    images: [String],
+    html: String,
+    created: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now },
+    url: String,
+    urls: [String]
+});
+mongoose.model('Url', UrlSchema);
+mongoose.connect('mongodb://localhost/linter');
 
-app.get('/*', function(req, res){
-    if (req.url.match(/^\/http.?:\/\/.*/)) {
-        url = req.url.replace(/^\//, "");
-        request(url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var document = jsdom.jsdom(body);
-                document.location = {};
-                url = response.request.uri.href;
-                document.location.href = url;
-                var result = ex.extract(document, url);
-                res.json({title: result.title, url:url, content:result.content.toString(), images: result.content.main_image(jsdom)});
-            }else{
-                return null;
-            }
-        });
-    }else{
-        res.writeHead(404, {'Content-Type':'application/json; charset=utf-8'});
-        res.end('Not found');
+app.enable('jsonp callback');
+app.get('/', function(req, res){
+    url = req.query.url ? req.query.url.replace(/^\//, "") : "";
+    if (!url.match(/^http.?:\/\/.*/)) {
+        return res.json({message: "Not found"+url, status: 404});
     }
+    Url = mongoose.model('Url');
+    Url.find({url: url}, function (err, docs) {
+        if (typeof docs[0] != "undefined") {
+            doc = docs[0];
+            return res.json({title: doc.title, url:doc.url, content:doc.content, images: doc.images});
+        }
+        request(url, function (error, response, body) {
+            if (error || response.statusCode != 200) {
+                return res.json({message: error, status: response.statusCode});
+            }
+            var document = jsdom.jsdom(body);
+            document.location = {};
+            url = response.request.uri.href;
+            document.location.href = url;
+            ex.addHandler(ex.factory.getHandler('Heuristics'));
+            var result = ex.extract(document, url);
+            var new_url = new Url({title: result.title, url: url, content:result.content.toString()});
+            new_url.urls.push(url);
+            var images = result.content.main_image(jsdom);
+            for (var i = 0; i <= images.length; i++){
+                new_url.images.push(images[i]);
+            }
+            new_url.save(function(err){
+                if (err) { console.log(err); }
+            });
+            res.json({title: result.title, url:url, content:result.content.toString(), images: result.content.main_image(jsdom)});
+        });
+    });
 });
 app.listen(3000);
