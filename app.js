a308e33180f676694deeaa8b9d0cc8aa9cdee885
Atsushi Nakatsugawa <atsushi@moongift.jp>
2012-02-01 14:12:48 +0900

    Dummy

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var ext = require('./extract-content-all'),
 
 // url = 'http://www.aichan.biz/html/html/h.html';
 // url = 'http://d.hatena.ne.jp/hokaccha/20110801/1312216630'
-// url = 'http://d.hatena.ne.jp/hokaccha/20110801/1312216630'
 
 var Schema   = mongoose.Schema;
 var UrlSchema = new Schema({
